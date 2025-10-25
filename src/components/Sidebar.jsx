e5e340819d1d5f9b223927a77cb1c5f15cdbf114
sagar praveen <168666276+sagar00praveen@users.noreply.github.com>
2025-10-25 10:26:35 +0530

    Refactor Sidebar component structure and comments

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 import { HomeIcon } from '@heroicons/react/24/solid';
 
 const Sidebar = () => {
-  // A simple array to represent the 4 inactive dot links
+ 
   const inactiveLinks = [1, 2, 3, 4];
 
   return (
     <aside className="w-20 flex flex-col items-center bg-white border-r py-6 space-y-6">
-      {/* Active Link */}
+   
       <button className="p-3 bg-gray-800 text-white rounded-xl shadow-md">
         <HomeIcon className="w-6 h-6" />
       </button>
 
-      {/* Inactive Links */}
+    
       {inactiveLinks.map((item) => (
         <div
           key={item}
